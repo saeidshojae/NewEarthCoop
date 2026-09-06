d90995e0ae0d34f428efe0947894fa1aa6b25c7f
saeid shojaei <s.s.sh.kia@gmail.com>
2026-09-07 01:27:21 +0330

    test: generalize deprecated reputation admin guard

--- tests/Feature/Reputation/DeprecatedElectionRuleAdminGuardContractTest.php
+++ tests/Feature/Reputation/DeprecatedElectionRuleAdminGuardContractTest.php
@@ -6,18 +6,20 @@ use Tests\TestCase;
 
 class DeprecatedElectionRuleAdminGuardContractTest extends TestCase
 {
-    public function test_deprecated_election_rules_cannot_be_reactivated_from_admin_policy_form(): void
+    public function test_deprecated_reputation_rules_cannot_be_reactivated_from_admin_policy_form(): void
     {
         $controller = file_get_contents(app_path('Http/Controllers/Admin/ReputationController.php'));
         $view = file_get_contents(resource_path('views/admin/system-settings/reputation/index.blade.php'));
 
-        $this->assertStringContainsString("private const DEPRECATED_RULE_KEYS = ['election_candidate', 'election_participated'];", $controller);
+        $this->assertStringContainsString('private const DEPRECATED_RULE_KEYS = [', $controller);
+        $this->assertStringContainsString("'election_candidate'", $controller);
+        $this->assertStringContainsString("'election_participated'", $controller);
         $this->assertStringContainsString('in_array($key, self::DEPRECATED_RULE_KEYS, true)', $controller);
         $this->assertStringContainsString('$rule->active = false;', $controller);
         $this->assertStringContainsString('$rule->convertible = false;', $controller);
 
-        $this->assertStringContainsString("\$isDeprecated = in_array(\$rule->key, ['election_candidate', 'election_participated'], true);", $view);
+        $this->assertStringContainsString('$deprecatedRuleKeys', $view);
         $this->assertStringContainsString('قاعده منسوخ؛ فقط برای سابقه نگهداری می‌شود', $view);
-        $this->assertStringContainsString('{{ $isDeprecated ? \'disabled\' : \'\' }}', $view);
+        $this->assertStringContainsString("{{ \$isDeprecated ? 'disabled' : '' }}", $view);
     }
 }
